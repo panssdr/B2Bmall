ad107b79b17f755e2333fbcbee0983a66186d3ee
panssdr <524105236@qq.com>
2021-11-05 21:09:49 +0800

    需求管理

--- src/views/admin/DemandManage.vue
+++ src/views/admin/DemandManage.vue
@@ -6,12 +6,12 @@
       <el-button class="el-icon-sort" type="text">发起询价</el-button>
       <el-button class="el-icon-circle-close" type="text">停止流转</el-button>
     </div>
-    <el-table :data="list" stripe style="width: 90%;margin: 0 auto" @row-click="getId">
+    <el-table :data="list" fit="false" style="width: 90%;margin: 0 auto" @row-click="getId" highlight-current-row   @current-change="handleCurrentChange">
       <el-table-column prop="id" label="需求ID"> </el-table-column>
       <el-table-column prop="customerID" label="客户ID"> </el-table-column>
       <el-table-column prop="goodsName" label="需求名称"> </el-table-column>
-      <el-table-column prop="specifications" label="需求规格"> </el-table-column>
-      <el-table-column prop="descript" label="需求描述" width="180"> </el-table-column>
+      <el-table-column prop="specifications" width="200px" label="需求规格"> </el-table-column>
+      <el-table-column prop="descript" label="需求描述" width="200"> </el-table-column>
       <el-table-column prop="price" label="需求数量"> </el-table-column>
       <el-table-column prop="amount" label="创建日期"> </el-table-column>
       <el-table-column prop="staffID" label="客服号"> </el-table-column>
@@ -35,26 +35,42 @@ export default {
   name: 'DemandManage',
   data(){
     return{
-      list:[{}]
+      list:[{}],
+      clickNumber:"",
+      currentRow:null,
     }
   },
   methods:{
     getId(row){
-      console.log(row.id)
+      this.clickNumber=row.getId
+    },
+     handleCurrentChange(val) {
+        this.currentRow = val;
+      }
+  },
+  computed:{
+    isClick(){
+      return this.currentNumber=this.currentNumber
     }
   },
   mounted(){
     Axios.get("/api/demand/deMan").then(res => {
       this.list=res.data
-      if( this.list.status=='0'){
-         this.list.status="待接单"
-      }else if( this.list.status==1){
-         this.list.status="正在处理"
-      }else if( this.list.status==2){
-         this.list.status="已报价"
-      }else if( this.list.status==4){
-         this.list.status="以结束"
+
+     for(let i=0;i<this.list.length;i++){
+        if( this.list[i].status=='0'){
+        this.list[i].status="待接单"
+
+      }else if( this.list[i].status==1){
+        this.list[i].status="正在处理"
+      }else if( this.list[i].status==2){
+         this.list[i].status="已报价"
+      }else if( this.list[i].status==4){
+        this.list[i].status="已结束"
       }
+     }
+
+
       }).catch( res => console.log(res))
   }
 }
